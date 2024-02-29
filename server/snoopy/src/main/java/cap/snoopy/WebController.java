932c9c170f8ed1f65d729432ba46ec5956bc2c04
R-Hambly-TS <55469422+R-Hambly-TS@users.noreply.github.com>
2024-02-29 10:56:21 -0400

    Add files via upload

---
+++ server/snoopy/src/main/java/cap/snoopy/WebController.java
@@ -0,0 +1,15 @@
+package cap;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+
+@Controller
+public class WebController {
+	@GetMapping("/index")
+	public String getTemplate(@RequestParam(name="name", required = false, defaultValue ="World") String name, Model model) {
+		model.addAttribute("name",name);
+		return "index";
+	}
+}
