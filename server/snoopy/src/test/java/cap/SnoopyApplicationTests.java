fd257d0876481eed00c66508ab3dafff19079877
R-Hambly-TS <55469422+R-Hambly-TS@users.noreply.github.com>
2024-04-07 02:31:12 -0300

    Add files via upload

---
+++ server/snoopy/src/test/java/cap/SnoopyApplicationTests.java
@@ -0,0 +1,14 @@
+package cap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import cap.server.SnoopyApplication;
+
+@SpringBootTest(classes = SnoopyApplication.class)
+class SnoopyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
